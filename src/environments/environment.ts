b3c2d2de79d4fd538bdc4df7211f09034b0c74ad
wilitch <wilitchurviola@gmail.com>
2023-04-19 08:53:06 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    /* Endereço da API de usuários */
+    apiUsuarios: "http://apousuarios1505-001-site1.gtempurl.com/",
+    /* Endereço da API de produtos */
+    apiProdutos: "http://apiprodutos1505-001-site1.atempurl.com/"
+};
+
+
